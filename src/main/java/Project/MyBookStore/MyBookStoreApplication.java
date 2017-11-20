7547aa0e00da969c85784b1df6f9a364a916b9db
JosiahSingh <Josiahsingh1996@gmail.com>
2017-11-20 15:18:42 +0200

    First commit

---
+++ src/main/java/Project/MyBookStore/MyBookStoreApplication.java
@@ -0,0 +1,12 @@
+package Project.MyBookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/Project/MyBookStore/MyBookStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package Project.MyBookStore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBookStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
